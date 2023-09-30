a5568d890bba86bcfba17bb171d1e581f8ac021d
push-panjali23 <pushpanjalisoni312@gmail.com>
2023-09-30 07:11:59 -0700

    learnning js

---
+++ index01.js
@@ -0,0 +1,22 @@
+
+    // it's also called a dom mainipulation
+    // javascript console API
+    // ways to print js in screen
+    alert("whats your name... ??");
+    prompt();
+// this is use to print msg in screen
+
+    document.write("welcome to learning js");
+    // print display screen
+    console.log("hello !....", 4+5 , "addition");
+    console.warn("this is warnnig");
+    console.error("this is an error");
+    //we can use (console.log , console.warn , console.error) to print your msg in console
+    // console.clear(" this is use to clear console screen");
+
+    container.innerHTML = "Hello !.... pushpanjali what do you want to learn in js ";
+    // innerHTML always use with id Like here is a container
+    click.innerHTML = "click Now";
+    // use.innerHTML="am i button??"
+    // this text is convertd button in a text
+
